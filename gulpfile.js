eefabc364433dcc0626a4d74681213fe482b7167
Derek Morash <derek@ablesense.com>
2017-10-27 09:39:18 -0300

    Some padding fixes. Added sourcemaps to demo build for better debugging. Dist build is still fully minified and no sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var settings = require("./package.json").settings;
 // Gulp plugin setup
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require("gulp-autoprefixer");
 var concat = require("gulp-concat");
 
@@ -23,11 +24,14 @@ var autoprefixerOptions = {
 gulp.task("compilesass", function() {
   return gulp
     .src(settings.sass_source)
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write())
     .pipe(concat(settings.css_name))
-    .pipe(gulp.dest(settings.css_dest))
-    .pipe(gulp.dest('./demo'));
+    .pipe(gulp.dest('./demo'))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest(settings.css_dest));
 });
 
 // Default gulp action when gulp is run
